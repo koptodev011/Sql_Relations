a9d1ba8758895290a4af999302130322f6fc7d59
Nehalkothale <107396461+Nehalkothale@users.noreply.github.com>
2024-08-29 18:46:35 +0530

    data base connection

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/login', function () {
     return view('welcome');
+});
+
+
+route::get('/',function (){
+    return view('login');
 });
