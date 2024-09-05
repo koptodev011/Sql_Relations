691d6d4618b1ebaa82af2259992dbe95690509af
Nehalkothale <107396461+Nehalkothale@users.noreply.github.com>
2024-09-05 16:10:55 +0530

    one-to-one-relation

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\StudentController;
+use App\Models\Student;
 use Illuminate\Support\Facades\Route;
 
 route::get('/',function (){
     return view('login');
 });
+
+
+// Route::resource('/student',StudentController::class);
+
+
+//One to one relation routes
+Route::get('/student', [StudentController::class, 'create']);
+Route::get('/contact', [ContactController::class, 'show']);
